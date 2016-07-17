48922f02b10e3b9d042b10b2af4e2a64e8c57fd3
vishal <vishalsenthilkumaran05@gmail.com>
2016-07-17 16:07:26 +0530

    Update and rename vv.c to vvv.c

---
+++ vvv.c
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class PrimeCheck
+{
+   public static void main(String args[])
+   {		
+	int temp;
+	boolean isPrime=true;
+	Scanner scan= new Scanner(System.in);
+	System.out.println("Enter a number for check:");
+
+	int num=scan.nextInt();
+	for(int i=2;i<=num/2;i++)
+	{
+           temp=num%i;
+	   if(temp==0)
+	   {
+	      isPrime=false;
+	      break;
+	   }
+	}
+
+	if(isPrime)
+	   System.out.println(num + " is Prime Number");
+	else
+	   System.out.println(num + " is not Prime Number");
+   }
+}
