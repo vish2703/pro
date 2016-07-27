25e9b0c0830fba73ea11bf8a54a482865385a188
vishal <vishalsenthilkumaran05@gmail.com>
2016-07-27 21:25:49 +0530

    Update v1.java

--- v1.java
+++ v1.java
@@ -4,12 +4,12 @@ public class factorial
 {
 static Scanner in=new Scanner(System.in);
 static int fact=1;
-static int i,n;
+static int i,s;
 
 public static void main(String[] args)
 {
-	n=in.nextInt();
-	for(i=1;i<=n;i++)
+	s=in.nextInt();
+	for(i=1;i<=s.legth;i++)
 	{
 		fact=fact*i;
 		
