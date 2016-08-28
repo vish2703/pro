488fd746bf72e5dabb9691ec93bd4dcb29c84a2d
vishal <vishalsenthilkumaran05@gmail.com>
2016-08-28 07:09:54 +0530

    Update visu.java

--- visu.java
+++ visu.java
@@ -1,10 +1,14 @@
 import java.util.*;
-public class rev
-{
-public static void main(String args[])
-{
-Scanner in=new Scanner(System.in)
-String s=in.nextLine();
-s=new StringBuffer(s.reverse.toString());
-System.out.println(s);
+public class StringReverseExample {
+ 
+  public static void main(String args[]){
+  
+    String strOriginal = "Hello World";
+    System.out.println("Original String : " + strOriginal);
+   
+    strOriginal = new StringBuffer(strOriginal).reverse().toString();
+    System.out.println("Reversed String : " + strOriginal);
+  }
+ 
 }
+ 
