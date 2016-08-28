ec4355375b939f744572f4ee2b7ae0f6e373347c
vishal <vishalsenthilkumaran05@gmail.com>
2016-08-28 07:06:35 +0530

    Create visu.java

---
+++ visu.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class rev
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in)
+String s=in.nextLine();
+s=new StringBuffer(s.reverse.toString());
+System.out.println(s);
+}
