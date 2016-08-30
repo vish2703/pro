b149925acb28592e197ccf90d5a278bbc08550da
vishal <vishalsenthilkumaran05@gmail.com>
2016-08-30 11:32:35 +0530

    Create v7.java

---
+++ v7.java
@@ -0,0 +1,26 @@
+package com.pro.guvi;
+
+import java.util.Scanner;
+
+public class alph 
+{
+  static Scanner in=new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		
+		int a = 0;
+		
+		int stack[]=new int[100];
+		
+		for(int i=0;i<stack.length;i++)
+		{
+			a=in.nextLine().charAt(0);
+			
+			System.out.println(a+" ");
+		}
+		
+		
+
+	}
+
+}
