bd732ac42165ac5daf4947309bc4a19df121b953
vishal <vishalsenthilkumaran05@gmail.com>
2016-09-02 12:14:56 +0530

    Update rv.java

--- rv.java
+++ rv.java
@@ -7,23 +7,23 @@ public static void main(String args[])throws Exception
 Scanner sc=new Scanner(System.in);
 int n,j;
 n=sc.nextInt();
-String av[]=new String[100];
+String a[]=new String[100];
 for(int i=0;i<n;i++)
 {
-av[i]=sc.nextLine();
+a[i]=sc.nextLine();
 }
 String long,check;
-if(av.length>0)
-long=av[0];
-for(int i=1;i<av.length;i++)
+if(a.length>0)
+long=a[0];
+for(int i=1;i<a.length;i++)
 {
-check=av[i];
-for(j=0;j<Math.min(long.length(),av[i].length());j++)
+check=a[i];
+for(j=0;j<Math.min(long.length(),a[i].length());j++)
 {
 if(long.charAt(j)!=check.charAt(j))
 break;
 }
-long=av[i].subString(0,j);
+long=a[i].subString(0,j);
 }
 System.out.println("longest prefix is:"+long);
 }
