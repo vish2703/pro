1e6dd881132e05793f0f45ffdb06c5b6b9865ab6
vishal <vishalsenthilkumaran05@gmail.com>
2016-09-02 09:34:58 +0530

    Create vt.java

---
+++ vt.java
@@ -0,0 +1,15 @@
+public class prime
+{
+public static void main(String args[])
+{
+int arr[]=new int[100];
+if(arr[]%2==0)
+{
+system.out.println("prime");
+int n[]=n[]+arr[];
+}
+else
+{
+system.ou.println("not prime");}
+System.out.println(n);
+}}
